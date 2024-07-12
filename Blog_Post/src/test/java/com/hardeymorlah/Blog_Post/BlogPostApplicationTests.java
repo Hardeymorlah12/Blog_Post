fcebf0067e3e11b5194064fe5139c9c4696b9680
Saheed Ademola Adewale <136191002+Hardeymorlah12@users.noreply.github.com>
2024-07-12 18:02:13 +0100

    Add files via upload

---
+++ Blog_Post/src/test/java/com/hardeymorlah/Blog_Post/BlogPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardeymorlah.Blog_Post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
